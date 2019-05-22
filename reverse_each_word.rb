6adb3b52e25ff8e0705222dbc4825af42e89e7bb
Peter Hwang <nam-joong.hwang@yale.edu>
2019-05-22 04:56:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(string)
-  string.scan(/./)
-
+  array = string.scan(/./)
+  reverse_array = array.reverse
+  new_array = []
+  reverse_array.each do |letter|
+    new_array << letter
+  end
 end
